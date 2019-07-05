cb65e31a471088477851105b2d0525cd37df6591
randsa4484 <randsa4484>
2019-07-05 23:03:41 +0000

    Initial

---
+++ src/ProjectPS.ServicePS.API/Controllers/BaseController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectPS.ServicePS.API.Controllers
+{
+    public abstract class BaseController : Controller
+    {
+      
+
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public IActionResult Conflict(object value)
+        {
+            return this.StatusCode(StatusCodes.Status409Conflict, value);
+        }
+
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public IActionResult Error(object value)
+        {
+            return this.StatusCode(StatusCodes.Status500InternalServerError, value);
+        }
+        
+    }
+}
---
+++ src/ProjectPS.ServicePS.Util.Options/AWSSettingOptions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ProjectPS.ServicePS.Util.Options
+{
+    public class AWSSettingOptions
+    {
+        public string AccessKey { get; set; }
+        public string SecretKey { get; set; }
+        public string Region { get; set; }
+    }
+}
